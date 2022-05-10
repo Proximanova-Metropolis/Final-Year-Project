f2988041a25873f373833e158a4f2d8a0a250ff1
Soham Bhadra <sohambhadra2000@gmail.com>
2022-05-11 01:11:19 +0800

    small change

--- pyFlaskProject/main.py
+++ pyFlaskProject/main.py
@@ -52,13 +52,13 @@ def home_page3():
     elif len(txt1) > 0:
         if sn > 0: retval1 = senticnet.sentiment_analyzer_scores(txt1)
         if swn > 0: retval2 = sentiwordnet.sentiment_analyzer_scores(txt1)
-        if vd >0: retval3 = vader.sentiment_analyzer_scores(txt1)
+        if vd > 0: retval3 = vader.sentiment_analyzer_scores(txt1)
     else:
         retval1 = ''
         retval2 = ''
         retval3 = ''
 
-    data_set = {'senticnet1': retval1, 'sentiwordnet1': retval2, 'vader1':retval3 }
+    data_set = {'senticnet1': retval1, 'sentiwordnet1': retval2, 'vader1':retval3}
     json_dump = json.dumps(data_set)
     return json_dump
 
